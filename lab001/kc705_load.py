2ba8712eb96aacf4ce0faeea13870ce7d6c6822e
8pins <40314150+8pins@users.noreply.github.com>
2022-05-12 21:18:42 +0700

    Update kc705_load.py

--- lab001/kc705_load.py
+++ lab001/kc705_load.py
@@ -4,7 +4,7 @@ import os
 
 # if you have djtgcfg installed, run this
 
-os.system("djtgcfg prog -d kc705 -i 0 -f ./build/top.bit")
+os.system("djtgcfg prog -d JtagSmt1 -i 0 -f ./solutions/build/top.bit")
 
 # if you have openocd installed, run this
 # from litex.build.openocd import OpenOCD
